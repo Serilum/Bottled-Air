74937b967333dcd671a4a36ee44b3687eefb4eba
Rick South <rick@natamus.com>
2023-12-12 10:48:45 +0100

    Bottled Air source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/bottledair/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.bottledair;
+
+import com.natamus.bottledair.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
