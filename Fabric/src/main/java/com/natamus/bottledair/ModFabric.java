3fd387ddb6464bfdf08a1256fe072ce1dd4bec16
Rick South <rick@natamus.com>
2023-12-12 10:48:51 +0100

    Bottled Air source code for 1.20.0.

---
+++ Fabric/src/main/java/com/natamus/bottledair/ModFabric.java
@@ -0,0 +1,30 @@
+package com.natamus.bottledair;
+
+import com.natamus.bottledair.events.AirEvent;
+import com.natamus.bottledair.util.Reference;
+import com.natamus.collective.check.RegisterMod;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		UseItemCallback.EVENT.register((player, world, hand) -> {
+			return AirEvent.onBottleClick(player, world, hand);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
