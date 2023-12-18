f175c75522b281715ab63e13794bd0091495be91
Rick South <rick@natamus.com>
2023-12-18 10:39:15 +0100

    (v2.3) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/bottledair/util/Reference.java
+++ Common/src/main/java/com/natamus/bottledair/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.bottledair.util;
 public class Reference {
 	public static final String MOD_ID = "bottledair";
 	public static final String NAME = "Bottled Air";
-	public static final String VERSION = "2.2";
+	public static final String VERSION = "2.3";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
