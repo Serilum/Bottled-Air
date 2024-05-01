642bb7e6e59f14ce16bda886d22d9ef542886e34
Rick South <rick@natamus.com>
2024-05-01 16:45:20 +0200

    Bottled Air source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/bottledair/neoforge/events/NeoForgeAirEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.bottledair.neoforge.events;
+
+import com.natamus.bottledair.events.AirEvent;
+import net.minecraft.world.InteractionResult;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeAirEvent {
+	@SubscribeEvent
+	public static void onBottleClick(PlayerInteractEvent.RightClickItem e) {
+		if (AirEvent.onBottleClick(e.getEntity(), e.getLevel(), e.getHand()).getResult().equals(InteractionResult.FAIL)) {
+			e.setCanceled(true);
+		}
+	}
+}
