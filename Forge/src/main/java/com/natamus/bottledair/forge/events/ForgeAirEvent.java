3ad2408e52b891ffc96807f6484aacc287278c10
Rick South <rick@natamus.com>
2024-06-14 05:41:02 +0200

    Bottled Air source code for 1.21.0.

---
+++ Forge/src/main/java/com/natamus/bottledair/forge/events/ForgeAirEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.bottledair.forge.events;
+
+import com.natamus.bottledair.events.AirEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeAirEvent {
+	@SubscribeEvent
+	public void onBottleClick(PlayerInteractEvent.RightClickItem e) {
+		if (AirEvent.onBottleClick(e.getEntity(), e.getLevel(), e.getHand()).getResult().equals(InteractionResult.FAIL)) {
+			e.setCanceled(true);
+		}
+	}
+}
