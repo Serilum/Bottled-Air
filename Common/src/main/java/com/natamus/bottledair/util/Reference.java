b943f37cd7df672ec3323f7b611764038de32219
Rick South <rick@natamus.com>
2026-06-19 00:09:16 +0200

    Updated Bottled Air to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/bottledair/util/Reference.java
+++ Common/src/main/java/com/natamus/bottledair/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "bottledair";
 	public static final String NAME = "Bottled Air";
 	public static final String VERSION = "2.5";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
